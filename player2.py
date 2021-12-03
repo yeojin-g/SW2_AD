b7745157cb1d425bdeb1291f46bbde99b636209e
bomi0320 <84120820+bomi0320@users.noreply.github.com>
2021-12-03 15:59:36 +0900

    messageOutput() Enter 버튼 누르라는 메시지 추가

--- player2.py
+++ player2.py
@@ -10,7 +10,7 @@ class Player2(Player):
 
     # player2이 수비자일 때 출력하는 메시지
     def messageOutput(self):
-        note = f"""참가번호 {self.number}번 {self.name}님의 수비입니다.\n이번 게임에 걸 구슬의 개수를 입력하세요.
+        note = f"""참가번호 {self.number}번 {self.name}님의 수비입니다.\n이번 게임에 걸 구슬의 개수를 입력한 다음 Enter 버튼을 눌러주세요.
                """
         return note
 
